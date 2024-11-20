32f07172177451a0127c28b8b42e9578b0bb65d0
sh0311 <0311alice@naver.com>
2024-11-20 19:12:53 +0900

    초기세팅

---
+++ src/main/java/ceos/phtotoground/PhtotogroundApplication.java
@@ -0,0 +1,13 @@
+package ceos.phtotoground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhtotogroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhtotogroundApplication.class, args);
+	}
+
+}
