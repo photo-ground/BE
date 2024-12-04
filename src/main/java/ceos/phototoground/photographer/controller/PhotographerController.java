775fafd249f7c657af9f8e57bfd5b6e5e3c1873e
sh0311 <0311alice@naver.com>
2024-12-04 21:56:29 +0900

    ✨ feat : 작가 프로필 조회 (상단)

--- src/main/java/ceos/phototoground/photographer/controller/PhotographerController.java
+++ src/main/java/ceos/phototoground/photographer/controller/PhotographerController.java
@@ -1,12 +1,14 @@
 package ceos.phototoground.photographer.controller;
 
 
+import ceos.phototoground.photographer.dto.PhotographerIntroDTO;
 import ceos.phototoground.photographer.dto.PhotographerListDTO;
 import ceos.phototoground.photographer.dto.PhotographerSearchListDTO;
 import ceos.phototoground.photographer.service.PhotographerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ public class PhotographerController {
 
     private final PhotographerService photographerService;
 
+    // 작가 리스트 조회 (필터링 포함)
     @GetMapping
     public ResponseEntity<PhotographerListDTO> getPhotographerList(
             @RequestParam(value = "cursor", required = false) Long cursor,
@@ -30,6 +33,7 @@ public class PhotographerController {
         return ResponseEntity.ok(dto);
     }
 
+    // 작가 검색
     @GetMapping("/search")
     public ResponseEntity<PhotographerSearchListDTO> searchPhotographer(
             @RequestParam(value = "name", required = false) String name,
@@ -42,4 +46,18 @@ public class PhotographerController {
 
     }
 
+    // 특정 작가 상단부 조회
+    @GetMapping("/{photographerId}/intro")
+    public ResponseEntity<PhotographerIntroDTO> getPhotographerIntro(@PathVariable Long photographerId) {
+
+        PhotographerIntroDTO dto = photographerService.getPhotographerIntro(photographerId);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    // 특정 작가 하단부 조회
+    @GetMapping("/{photographerId}/bottom")
+    public ResponseEntity<PhotographerBottomDTO> getPhotographerBottom(@PathVariable Long photographerId) {
+
+    }
 }
--- src/main/java/ceos/phototoground/photographer/service/PhotographerService.java
+++ src/main/java/ceos/phototoground/photographer/service/PhotographerService.java
@@ -5,10 +5,13 @@ import ceos.phototoground.photoProfile.domain.QPhotoProfile;
 import ceos.phototoground.photoProfile.service.PhotoProfileService;
 import ceos.phototoground.photographer.domain.Photographer;
 import ceos.phototoground.photographer.domain.QPhotographer;
+import ceos.phototoground.photographer.dto.PhotographerIntroDTO;
 import ceos.phototoground.photographer.dto.PhotographerListDTO;
 import ceos.phototoground.photographer.dto.PhotographerResponseDTO;
 import ceos.phototoground.photographer.dto.PhotographerSearchListDTO;
 import ceos.phototoground.photographer.repository.PhotographerRepository;
+import ceos.phototoground.univ.domain.PhotographerUniv;
+import ceos.phototoground.univ.service.PhotographerUnivService;
 import com.querydsl.core.Tuple;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +26,7 @@ public class PhotographerService {
 
     private final PhotographerRepository photographerRepository;
     private final PhotoProfileService photoProfileService;
+    private final PhotographerUnivService photographerUnivService;
 
     @Transactional
     public Photographer findPhotographerById(Long photographerId) {
@@ -81,5 +85,21 @@ public class PhotographerService {
 
         return PhotographerSearchListDTO.of(dtos, hasNext, nextCursor);
 
+    }
+
+    public PhotographerIntroDTO getPhotographerIntro(Long photographerId) {
+
+        Photographer photographer = photographerRepository.findById(photographerId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 id의 작가가 존재하지 않습니다."));
+
+        PhotoProfile photoProfile = photographer.getPhotoProfile();
+
+        List<PhotographerUniv> photographerUnivList = photographerUnivService.findByPhotographer_Id(photographerId);
+
+        List<String> univNameList = photographerUnivList.stream()
+                .map(photographerUniv -> photographerUniv.getUniv().getName())
+                .toList();
+
+        return PhotographerIntroDTO.of(photographer, photoProfile, univNameList);
     }
 }
