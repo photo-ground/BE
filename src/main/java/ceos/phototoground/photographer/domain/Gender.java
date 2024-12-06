27b6c6b23c77d74b08f32709c01654e1e6b24fe5
seona-moon <fhflwhwl5@gmail.com>
2024-12-07 00:43:00 +0900

    🔧chore: 이넘 타입 값 수정

--- src/main/java/ceos/phototoground/photographer/domain/Gender.java
+++ src/main/java/ceos/phototoground/photographer/domain/Gender.java
@@ -1,5 +1,14 @@
 package ceos.phototoground.photographer.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Gender {
-    FEMALE, MALE
+    MALE("남성"),
+    FEMALE("여성");
+
+    private final String name;
 }
