a7e5b93994e76d97dd94ef4185ac554cc9a8e002
seona-moon <fhflwhwl5@gmail.com>
2024-12-07 10:39:56 +0900

    ✨ feat: CORS 설정

--- src/main/java/ceos/phototoground/MainController.java
+++ src/main/java/ceos/phototoground/MainController.java
@@ -1,5 +1,10 @@
 package ceos.phototoground;
 
+import java.util.Collection;
+import java.util.Iterator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +16,15 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "main Controller";
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller : "+name + role;
     }
 }
--- src/main/java/ceos/phototoground/global/config/SecurityConfig.java
+++ src/main/java/ceos/phototoground/global/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import ceos.phototoground.global.jwt.JWTFilter;
 import ceos.phototoground.global.jwt.JWTUtil;
 import ceos.phototoground.global.jwt.LoginFilter;
 import io.jsonwebtoken.Jwt;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity //Security config 설정
@@ -41,6 +45,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //cors 설정
+        http.cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         //csrf disable
         http.csrf((auth) -> auth.disable());
