06d120be54b16d269c985638db1abc62484e0f97
seona-moon <fhflwhwl5@gmail.com>
2024-12-20 01:43:42 +0900

    ✨ feat: 로그아웃 기능 구현

--- src/main/java/ceos/phototoground/global/config/SecurityConfig.java
+++ src/main/java/ceos/phototoground/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package ceos.phototoground.global.config;
 
+import ceos.phototoground.global.entity.RefreshRepository;
+import ceos.phototoground.global.jwt.CustomLogoutFilter;
 import ceos.phototoground.global.jwt.JWTFilter;
 import ceos.phototoground.global.jwt.JWTUtil;
 import ceos.phototoground.global.jwt.LoginFilter;
@@ -17,6 +19,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -28,6 +31,7 @@ public class SecurityConfig {
     //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
     //AuthenticationManager Bean 등록
     @Bean
@@ -76,19 +80,21 @@ public class SecurityConfig {
 
         //경로별 인가 작업
         http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/login", "/", "/api/customer/join", "/env")
-                .permitAll() // 해당 경로는 모든 사용자가 접근 가능
-                .requestMatchers("/admin")
-                .hasRole("ADMIN") // admin 경로는 해당 권한을 가진 사용자만 접근 가능.
-                .anyRequest()
-                .authenticated()); // 이외의 남은 경로는 로그인한 사용자만 접근 가능
+                .requestMatchers("/login", "/", "/api/customer/join", "/env").permitAll() // 해당 경로는 모든 사용자가 접근 가능
+                .requestMatchers("/admin").hasRole("ADMIN") // admin 경로는 해당 권한을 가진 사용자만 접근 가능.
+                .requestMatchers("/api/reissue").permitAll() // 리프레시 토큰은 모든 사용자가 접근 가능
+                .anyRequest().authenticated()); // 이외의 남은 경로는 로그인한 사용자만 접근 가능
 
         // JWT 권한 검증
         http.addFilterAfter(new JWTFilter(jwtUtil), LoginFilter.class);
 
         // 로그인 필터 설정 (/login)
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository),
                 UsernamePasswordAuthenticationFilter.class);
+
+        // 로그아웃 필터 설정 (/logout)
+        http
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
 
         //세션 설정 (JWT는 세션이 stateless 상태로 설정되어야 함)
         http.sessionManagement((session) -> session
