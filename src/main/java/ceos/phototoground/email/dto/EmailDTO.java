8aea83b9b28c0efa2dee93008409e56f90843f76
sh0311 <0311alice@naver.com>
2024-12-22 14:41:29 +0900

    ♻️ refactor : 이메일 알림 문구 수정

--- src/main/java/ceos/phototoground/email/dto/EmailDTO.java
+++ src/main/java/ceos/phototoground/email/dto/EmailDTO.java
@@ -14,14 +14,14 @@ public class EmailDTO {
     public EmailDTO(Customer customer, Photographer photographer) {
 
         this.subject = "포토그라운드 예약 신청 알림";
-        this.text = customer.getId() + " id의 " + customer.getName() + " 고객님께서 " + photographer.getId()
-                + " id의 작가님께 예약 신청 하셨습니다.";
+        this.text = customer.getId() + " 번 id의 " + customer.getName() + " 고객님께서 " + photographer.getId()
+                + " 번 id의 작가님께 예약 신청 하셨습니다.";
     }
 
     public EmailDTO(Reservation reservation) {
 
         this.subject = "포토그라운드 예약 취소 알림";
-        this.text = reservation.getCustomer().getId() + " id의 고객님께서" + reservation.getPhotographer().getId()
-                + " id의 작가님 예약을 취소하셨습니다.";
+        this.text = reservation.getCustomer().getId() + " 번 id의 고객님께서" + reservation.getPhotographer().getId()
+                + " 번 id의 작가님 예약을 취소하셨습니다. 취소된 예약 id는 " + reservation.getId() + " 번 입니다.";
     }
 }
