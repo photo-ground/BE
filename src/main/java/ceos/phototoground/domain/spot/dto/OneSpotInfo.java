75e602eac0f68448284ace0a56eb821396adf62d
sh0311 <0311alice@naver.com>
2025-01-06 22:52:47 +0900

    ✨ feat : 특정 포토스팟 게시글 조회 반환값 수정

--- src/main/java/ceos/phototoground/domain/spot/dto/OneSpotInfo.java
+++ src/main/java/ceos/phototoground/domain/spot/dto/OneSpotInfo.java
@@ -12,6 +12,7 @@ public class OneSpotInfo {
     private Long spotId;
     private String spotName;
     private String content;
+    private String univName;
     private SpotPostImageList imageInfo;
 
 
@@ -20,6 +21,7 @@ public class OneSpotInfo {
                 .spotId(spot.getId())
                 .spotName(spot.getName())
                 .content(spot.getContent())
+                .univName(spot.getUniv().getName())
                 .imageInfo(imageList)
                 .build();
     }
