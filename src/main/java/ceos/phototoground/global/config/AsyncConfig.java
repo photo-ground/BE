314c3098fe69193b9d1bb680945b34a6e0484519
sh0311 <0311alice@naver.com>
2025-01-14 22:12:40 +0900

    chore: pool size 줄임

--- src/main/java/ceos/phototoground/global/config/AsyncConfig.java
+++ src/main/java/ceos/phototoground/global/config/AsyncConfig.java
@@ -1,21 +1,20 @@
 package ceos.phototoground.global.config;
 
+import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
-
 @EnableAsync
 @Configuration
 public class AsyncConfig {
-    @Bean(name="ImageUploadExecutor")
+    @Bean(name = "ImageUploadExecutor")
     public Executor imageUploadExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(9);
-        executor.setMaxPoolSize(18);
-        executor.setQueueCapacity(100);
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(20);
         executor.setThreadNamePrefix("ImageUpload-");
         executor.initialize();
         return executor;
