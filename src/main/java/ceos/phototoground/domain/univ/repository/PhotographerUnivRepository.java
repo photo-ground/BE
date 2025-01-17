dcdc5783963b20e7417b597c1d6cfdf62b4ed236
sh0311 <0311alice@naver.com>
2025-01-17 10:22:56 +0900

    🚑 hotfix : 작가 대학교 jpql 수정

--- src/main/java/ceos/phototoground/domain/univ/repository/PhotographerUnivRepository.java
+++ src/main/java/ceos/phototoground/domain/univ/repository/PhotographerUnivRepository.java
@@ -4,11 +4,12 @@ import ceos.phototoground.domain.univ.entity.PhotographerUniv;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PhotographerUnivRepository extends JpaRepository<PhotographerUniv, Long> {
 
-    @Query("SELECT p FROM PhotographerUniv p JOIN FETCH p.univ")
-    List<PhotographerUniv> findByPhotographer_Id(Long photographerId);
+    @Query("SELECT p FROM PhotographerUniv p JOIN FETCH p.univ WHERE p.photographer.id = :photographerId")
+    List<PhotographerUniv> findByPhotographer_Id(@Param("photographerId") Long photographerId);
 }
