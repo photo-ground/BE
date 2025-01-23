1d4aa21e74de301faf544abe4ecd04e562821dfc
sh0311 <0311alice@naver.com>
2025-01-23 14:33:50 +0900

    fix : univ 즉시로딩

--- src/main/java/ceos/phototoground/domain/photographer/repository/PhotographerRepositoryImpl.java
+++ src/main/java/ceos/phototoground/domain/photographer/repository/PhotographerRepositoryImpl.java
@@ -5,7 +5,6 @@ import ceos.phototoground.domain.photoProfile.entity.QPhotoProfile;
 import ceos.phototoground.domain.photographer.entity.Photographer;
 import ceos.phototoground.domain.photographer.entity.QPhotographer;
 import ceos.phototoground.domain.univ.entity.QPhotographerUniv;
-import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -19,46 +18,27 @@ public class PhotographerRepositoryImpl implements PhotographerRepositoryCustom 
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<Tuple> findPhotographerWithNoOffset(Long cursor, int size, String univ, String gender) {
+    public List<Photographer> findPhotographerWithNoOffset(Long cursor, int size, String univ, String gender) {
 
         QPhotographer photographer = QPhotographer.photographer;
         QPhotoProfile photoProfile = QPhotoProfile.photoProfile;
         QPhotographerUniv photographerUniv = QPhotographerUniv.photographerUniv;
 
         System.out.println("쿼리DSL size : " + size);
-/*
+
         List<Photographer> photographers = jpaQueryFactory
                 .selectDistinct(photographer)
                 .from(photographer)
                 .leftJoin(photographer.photoProfile, photoProfile).fetchJoin()
-                .leftJoin(photographerUniv).fetchJoin()
-                .on(photographer.id.eq(photographerUniv.photographer.id)) //한 번의 쿼리로 photographerUniv 까지 가져오려고
+                .leftJoin(photographerUniv).on(photographer.id.eq(photographerUniv.photographer.id))
+                .fetchJoin()//한 번의 쿼리로 photographerUniv 까지 가져오려고
                 .leftJoin(photographerUniv.univ)
                 .where(eqUniv(univ, photographerUniv), eqGender(gender, photographer), ltCursorId(cursor, photographer))
                 .orderBy(photographer.id.desc())
                 .limit(size)
                 .fetch();
-*/
 
-        List<Photographer> photographers = jpaQueryFactory
-                .selectDistinct(photographer)
-                .from(photographer)
-                .leftJoin(photographerUniv)
-                .on(photographer.id.eq(photographerUniv.photographer.id)) //한 번의 쿼리로 photographerUniv 까지 가져오려고
-                .where(eqUniv(univ, photographerUniv), eqGender(gender, photographer), ltCursorId(cursor, photographer))
-                .orderBy(photographer.id.desc())
-                .limit(size)
-                .fetch();
-
-        return jpaQueryFactory
-                .select(photographer, photographerUniv)
-                .from(photographer)
-                .leftJoin(photographer.photoProfile, photoProfile).fetchJoin()
-                .leftJoin(photographerUniv).on(photographer.id.eq(photographerUniv.photographer.id)).fetchJoin()
-                .leftJoin(photographerUniv.univ).fetchJoin()
-                .where(photographer.in(photographers))
-                .fetch();
-
+        return photographers;
     }
 
     //학교필터링
--- src/main/java/ceos/phototoground/domain/univ/entity/PhotographerUniv.java
+++ src/main/java/ceos/phototoground/domain/univ/entity/PhotographerUniv.java
@@ -1,29 +1,40 @@
 package ceos.phototoground.domain.univ.entity;
 
-import ceos.phototoground.global.entity.BaseTimeEntity;
 import ceos.phototoground.domain.photographer.entity.Photographer;
-import jakarta.persistence.*;
-import lombok.*;
+import ceos.phototoground.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 //작가, 촬영가능 대학교 연관테이블
 @Entity
 @Getter
-@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor
 public class PhotographerUniv extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="photographer_univ_id")
+    @Column(name = "photographer_univ_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="photographer_id")
+    @JoinColumn(name = "photographer_id")
     private Photographer photographer;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="univ_id")
+    @ManyToOne
+    @JoinColumn(name = "univ_id")
     private Univ univ;
 
 }
