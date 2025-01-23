70232684bf120b401e5344e843081f2a584e7da4
sh0311 <0311alice@naver.com>
2025-01-23 13:53:03 +0900

    fix : univ fetchJoin

--- src/main/java/ceos/phototoground/domain/photographer/repository/PhotographerRepositoryImpl.java
+++ src/main/java/ceos/phototoground/domain/photographer/repository/PhotographerRepositoryImpl.java
@@ -5,6 +5,7 @@ import ceos.phototoground.domain.photoProfile.entity.QPhotoProfile;
 import ceos.phototoground.domain.photographer.entity.Photographer;
 import ceos.phototoground.domain.photographer.entity.QPhotographer;
 import ceos.phototoground.domain.univ.entity.QPhotographerUniv;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -18,14 +19,14 @@ public class PhotographerRepositoryImpl implements PhotographerRepositoryCustom 
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<Photographer> findPhotographerWithNoOffset(Long cursor, int size, String univ, String gender) {
+    public List<Tuple> findPhotographerWithNoOffset(Long cursor, int size, String univ, String gender) {
 
         QPhotographer photographer = QPhotographer.photographer;
         QPhotoProfile photoProfile = QPhotoProfile.photoProfile;
         QPhotographerUniv photographerUniv = QPhotographerUniv.photographerUniv;
 
         System.out.println("쿼리DSL size : " + size);
-
+/*
         List<Photographer> photographers = jpaQueryFactory
                 .selectDistinct(photographer)
                 .from(photographer)
@@ -37,8 +38,26 @@ public class PhotographerRepositoryImpl implements PhotographerRepositoryCustom 
                 .orderBy(photographer.id.desc())
                 .limit(size)
                 .fetch();
+*/
 
-        return photographers;
+        List<Photographer> photographers = jpaQueryFactory
+                .selectDistinct(photographer)
+                .from(photographer)
+                .leftJoin(photographerUniv)
+                .on(photographer.id.eq(photographerUniv.photographer.id)) //한 번의 쿼리로 photographerUniv 까지 가져오려고
+                .where(eqUniv(univ, photographerUniv), eqGender(gender, photographer), ltCursorId(cursor, photographer))
+                .orderBy(photographer.id.desc())
+                .limit(size)
+                .fetch();
+
+        return jpaQueryFactory
+                .select(photographer, photographerUniv)
+                .from(photographer)
+                .leftJoin(photographer.photoProfile, photoProfile).fetchJoin()
+                .leftJoin(photographerUniv).on(photographer.id.eq(photographerUniv.photographer.id)).fetchJoin()
+                .leftJoin(photographerUniv.univ).fetchJoin()
+                .where(photographer.in(photographers))
+                .fetch();
 
     }
 
